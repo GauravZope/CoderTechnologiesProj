fdbfd2cfc8036346b6af15c59fbdb5cee1df39fc
gaurav <gauravzope.1s1k@gmail.com>
2018-01-20 20:45:44 +0530

    Hibernate initial import V1.0

--- src/main/java/in/codertechnolgies/batchschedule/DaoImpl/StudentInformationDaoImpl.java
+++ src/main/java/in/codertechnolgies/batchschedule/DaoImpl/StudentInformationDaoImpl.java
@@ -1,7 +1,21 @@
 package in.codertechnolgies.batchschedule.DaoImpl;
 
-import in.codertechnolgies.batchschedule.Dao.StudentInformationDao;
+import javax.transaction.Transactional;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import in.codertechnolgies.batchschedule.Dao.StudentInformationDao;
+@Repository
+@Transactional
 public class StudentInformationDaoImpl implements StudentInformationDao {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	public void saveStateMstService(StateMst stateMst) {
+		sessionFactory.getCurrentSession().saveOrUpdate(stateMst);
+	}
 }
